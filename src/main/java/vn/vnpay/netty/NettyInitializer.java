b9536e8b27d94a0cff6f2a569c948013408ca976
quanghuyy86 <taquanghuy147@gmail.com>
2024-10-14 16:17:35 +0700

    #4 - add redis

--- src/main/java/vn/vnpay/netty/NettyInitializer.java
+++ src/main/java/vn/vnpay/netty/NettyInitializer.java
@@ -6,6 +6,8 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import redis.clients.jedis.JedisPool;
+import vn.vnpay.config.redis.RedisConfig;
 import vn.vnpay.service.PaymentService;
 import vn.vnpay.service.impl.PaymentServiceImpl;
 
@@ -19,7 +21,8 @@ public class NettyInitializer extends ChannelInitializer<SocketChannel> {
         pipeline.addLast(new HttpObjectAggregator(65536));
         pipeline.addLast(new ChunkedWriteHandler());
 
-        PaymentService paymentService = new PaymentServiceImpl();
+        JedisPool jedisPool = new JedisPool();
+        PaymentService paymentService = new PaymentServiceImpl(jedisPool);
         pipeline.addLast(new NettyHandler(paymentService));
     }
 }
